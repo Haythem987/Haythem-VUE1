c65f7f7a12cbfe3ff3866764ca8384f3fe09131b
Haythem <3030746629@qq.com>
2025-02-04 16:42:36 +0800

    element-plus的安装

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'unocss'
+import {defineConfig, presetWind} from 'unocss'
 
 export default defineConfig({
     // ...UnoCSS options
+    presets: [presetWind({important: '#__nuxt'})],
 })
